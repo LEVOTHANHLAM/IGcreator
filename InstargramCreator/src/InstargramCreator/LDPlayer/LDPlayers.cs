7ad15675cc1237fe4782b5b2e62df50fc74dd0f9
LEVOTHANHLAM <115397158+LEVOTHANHLAM@users.noreply.github.com>
2023-04-10 03:30:51 +0100

    10-04-2023

---
+++ InstargramCreator/src/InstargramCreator/LDPlayer/LDPlayers.cs
@@ -0,0 +1,36 @@
+﻿using InstargramCreator.Mains;
+using InstargramCreator.Repositories;
+
+namespace AppAuto.LDPlayer
+{
+    public class LDPlayers
+    {
+        LdCmd ldCmd = new LdCmd();       
+        public BindingSource soureLDPlayer = new BindingSource();
+        IAccountRepository _accountRepository;
+        public LDPlayers(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        public void LoadLdPlayer(List<MainAutoRun> ListDevices)
+        {
+            try
+            {
+                string listdevice = ldCmd.RunEmuConsole("list2");
+                ListDevices.Clear();
+                string[] devicelist = listdevice.Split('|');
+                foreach (string deviceitem in devicelist)
+                {
+                    if (string.IsNullOrEmpty(deviceitem)) continue;
+                    string[] s = deviceitem.Split(',');
+                    MainAutoRun driver = new MainAutoRun(_accountRepository);
+                    driver.Index = int.Parse(s[0]);
+                    driver.name = s[0];
+                    ListDevices.Add(driver);
+                }
+                soureLDPlayer.DataSource = ListDevices;
+            }
+            catch (Exception ex) { Serilog.Log.Error(ex.ToString()); }          
+        }
+    }
+}
